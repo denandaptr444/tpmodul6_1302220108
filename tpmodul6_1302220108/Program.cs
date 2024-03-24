e61c8c77b994ed5e664a59bf6c548e9dc893bbc2
Hp <Hp@LAPTOP-P71GJDB6>
2024-03-25 03:23:46 +0700

    Add project files.

---
+++ tpmodul6_1302220108/Program.cs
@@ -0,0 +1,24 @@
+﻿using tpmodul6_1302220108;
+
+
+namespace tpmodul6_1302220108;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Masukkan judul video: ");
+        string title = Console.ReadLine();
+
+        var video = new SayaTubeVideo(title);
+
+        Console.Write("Masukkan jumlah penambahan play count: ");
+        int increaseAmount = int.Parse(Console.ReadLine());
+
+        video.IncreasePlayCount(increaseAmount);
+
+        Console.Write("Masukkan Deskripsi: ");
+        string customOutput = Console.ReadLine();
+
+        video.PrintVideoDetails();
+    }
+}
---
+++ tpmodul6_1302220108/SayaTubeVideo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul6_1302220108
+{
+    internal class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+        public SayaTubeVideo(string title)
+        {
+            this.id = new Random().Next(10000, 99999);
+            this.title = title;
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int increaseAmount)
+        {
+            if (string.IsNullOrEmpty(title) || title.Length > 100)
+            {
+                throw new ArgumentException("Judul video tidak boleh null dan maksimal 100 karakter");
+            }
+
+            if (increaseAmount > 10000000)
+            {
+                throw new ArgumentException("Input penambahan play count maksimal 10.000.000");
+            }
+
+            try
+            {
+                checked
+                {
+                    playCount += increaseAmount;
+                }
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Jumlah play count melebihi batas!");
+            }
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine($"ID: {id}");
+            Console.WriteLine($"Judul: {title}");
+            Console.WriteLine($"Play Count: {playCount}");
+        }
+    }
+}
